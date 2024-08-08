67f6be2a8b5e515121a769c0d619afcd97671ba5
dbecher84 <dbecher84@gmail.com>
2024-08-08 10:35:27 -0400

    paccache folder
    Added ability to clear the paccache folder

--- clean_adesk_collab_locs/inputForm.Designer.cs
+++ clean_adesk_collab_locs/inputForm.Designer.cs
@@ -39,12 +39,10 @@ namespace clean_adesk_collab_locs
             this.checkBox_2023 = new System.Windows.Forms.CheckBox();
             this.checkBoxCefCache = new System.Windows.Forms.CheckBox();
             this.checkBoxCollabCache = new System.Windows.Forms.CheckBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.checkBox_2025 = new System.Windows.Forms.CheckBox();
             this.checkBox_2024 = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.buttonPAC = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
@@ -67,7 +65,7 @@ namespace clean_adesk_collab_locs
             // 
             this.buttonP3d.BackColor = System.Drawing.SystemColors.ControlLight;
             this.buttonP3d.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonP3d.Location = new System.Drawing.Point(179, 370);
+            this.buttonP3d.Location = new System.Drawing.Point(179, 360);
             this.buttonP3d.Name = "buttonP3d";
             this.buttonP3d.Size = new System.Drawing.Size(97, 38);
             this.buttonP3d.TabIndex = 3;
@@ -80,7 +78,7 @@ namespace clean_adesk_collab_locs
             this.buttonClose.BackColor = System.Drawing.SystemColors.ControlLight;
             this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.buttonClose.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonClose.Location = new System.Drawing.Point(340, 447);
+            this.buttonClose.Location = new System.Drawing.Point(358, 403);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(97, 38);
             this.buttonClose.TabIndex = 4;
@@ -164,19 +162,6 @@ namespace clean_adesk_collab_locs
             this.checkBoxCollabCache.UseVisualStyleBackColor = true;
             this.checkBoxCollabCache.CheckedChanged += new System.EventHandler(this.checkBoxCollabCache_CheckedChanged);
             // 
-            // textBox3
-            // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(12, 430);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(237, 55);
-            this.textBox3.TabIndex = 11;
-            this.textBox3.Text = "Note: If experiencing errors with a Revit file try clearing only the CefCache. If" +
-    " errors persist, Clear the Collaboration Cache.";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -193,7 +178,7 @@ namespace clean_adesk_collab_locs
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(81, 318);
+            this.label2.Location = new System.Drawing.Point(81, 324);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(297, 25);
             this.label2.TabIndex = 13;
@@ -221,7 +206,6 @@ namespace clean_adesk_collab_locs
             this.checkBox_2024.TabIndex = 14;
             this.checkBox_2024.Text = "2024";
             this.checkBox_2024.UseVisualStyleBackColor = true;
-            // 
             // 
             // textBox2
             // 
@@ -281,17 +265,15 @@ namespace clean_adesk_collab_locs
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(467, 497);
+            this.ClientSize = new System.Drawing.Size(467, 453);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.buttonPAC);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.checkBox_2025);
             this.Controls.Add(this.checkBox_2024);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.checkBoxCollabCache);
             this.Controls.Add(this.checkBoxCefCache);
             this.Controls.Add(this.checkBox_2023);
@@ -323,12 +305,10 @@ namespace clean_adesk_collab_locs
         private System.Windows.Forms.CheckBox checkBox_2023;
         private System.Windows.Forms.CheckBox checkBoxCefCache;
         private System.Windows.Forms.CheckBox checkBoxCollabCache;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox checkBox_2025;
         private System.Windows.Forms.CheckBox checkBox_2024;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button buttonPAC;
         private System.Windows.Forms.Label label3;
